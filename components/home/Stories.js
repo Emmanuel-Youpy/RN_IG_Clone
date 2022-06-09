aeaed67ca8b9f4f5f781f6331103ffbdb0fce228
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-06-09 10:07:34 +0800

    Stories component completed

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -10,9 +10,28 @@ const Stories = () => {
       }}
     >
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <Image source />
+        {USERS.map((story, index) => (
+          <View key={index} style={{ alignItems: "center" }}>
+            <Image
+              source={{ uri: story.image }}
+              style={{
+                width: 70,
+                height: 70,
+                borderRadius: 50,
+                marginLeft: 8,
+                borderWidth: 3,
+                borderColor: "#ff8501",
+              }}
+            />
+            <Text style={{ color: "white" }}>
+              {" "}
+              {story.user.length > 9
+                ? story.user.slice(0, 10).toLowerCase() + "..."
+                : story.user.toLowerCase()}
+            </Text>
+          </View>
+        ))}
       </ScrollView>
-      <Text style={{ color: "white" }}> Okay</Text>
     </View>
   );
 };
