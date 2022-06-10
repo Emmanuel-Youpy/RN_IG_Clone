2364580544685a59c70bc815830be5ac84b208f7
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-06-10 12:41:32 +0800

    Post component 1

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { Divider } from "react-native-elements";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <View>
+    <View style={{ marginBottom: 30 }}>
+      <Divider width={1} orientation="vertical" />
+      <PostHeader post={post} />
       <Text style={{ color: "white" }}>post</Text>
     </View>
   );
 };
 
+const PostHeader = ({ post }) => (
+  <View>
+    <View>
+      <Image />
+      <Text>Okay</Text>
+    </View>
+  </View>
+);
 export default Post;
